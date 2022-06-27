7cb4749f6945ff88b516ccc36c7fff3e2b879692
tsuyamakazuki <kazuki_tsuyama@bita.jp>
2022-06-27 17:24:25 +0900

    implement object state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { useState } from 'react'
 
 const App = props => {
+  const [state, setState] = useState(props)
+  const { name, price } = state
 
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
-
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
-  }
   return (
     <>
       <p>現在の{name}は、{price}円です</p>
-      <button onClick={()=>setPrice(price + 1)}>+1</button>
-      <button onClick={()=>setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={name} onChange={(event)=>setName(event.target.value)} />
+      <button onClick={()=>setState({...state, price: price + 1})}>+1</button>
+      <button onClick={()=>setState({...state, price: price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>Reset</button>
+      <input value={name} onChange={(event)=>setState({...state, name: event.target.value})} />
     </>
   )
 }
